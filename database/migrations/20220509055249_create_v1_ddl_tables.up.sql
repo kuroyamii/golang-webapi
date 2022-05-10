9d05dfe9fc2b54564d43792bcb5ae42777bea4bb
kuroyamii <gerysastrawan123@gmail.com>
2022-05-10 21:34:54 +0800

    Update database migrations

--- database/migrations/20220509055249_create_v1_ddl_tables.up.sql
+++ database/migrations/20220509055249_create_v1_ddl_tables.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS seat(
-	table_id INT,
+	table_id INT NOT NULL AUTO_INCREMENT,
 	status TINYINT,
 	PRIMARY KEY (table_id)
 );
@@ -11,6 +11,8 @@ CREATE TABLE IF NOT EXISTS customer(
 	table_id INT,
 	PRIMARY KEY (customer_id),
 	FOREIGN KEY (table_id) REFERENCES seat(table_id)
+	ON DELETE CASCADE
+	ON UPDATE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS orders(
@@ -19,6 +21,8 @@ CREATE TABLE IF NOT EXISTS orders(
 	ordered_at DATETIME,
 	PRIMARY KEY (order_id),
 	FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+	ON DELETE CASCADE
+	ON UPDATE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS food_type(
@@ -38,6 +42,8 @@ CREATE TABLE IF NOT EXISTS food(
 	stock INT,
 	PRIMARY KEY (food_id),
 	FOREIGN KEY (food_type_id) REFERENCES food_type(food_type_id)
+	ON DELETE CASCADE
+	ON UPDATE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS order_detail(
@@ -45,8 +51,12 @@ CREATE TABLE IF NOT EXISTS order_detail(
 	order_id BIGINT,
 	food_id INT,
 	PRIMARY KEY (details_id),
-	FOREIGN KEY (order_id) REFERENCES orders(order_id),
+	FOREIGN KEY (order_id) REFERENCES orders(order_id)
+	ON DELETE CASCADE
+	ON UPDATE NO ACTION,
 	FOREIGN KEY (food_id) REFERENCES food(food_id)
+	ON DELETE CASCADE
+	ON UPDATE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS records(
@@ -55,4 +65,6 @@ CREATE TABLE IF NOT EXISTS records(
 	amount INT,
 	PRIMARY KEY (record_id),
 	FOREIGN KEY (food_id) REFERENCES food(food_id)
+	ON DELETE CASCADE
+	ON UPDATE NO ACTION
 );
